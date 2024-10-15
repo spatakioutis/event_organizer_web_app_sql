25feb412fbb5ccbf0a1a188dd2a43565ce06a2d8
sofok <sofoklis2314@gmail.com>
2024-10-16 00:14:05 +0300

    add: sql table creations

---
+++ backend/database.sql
@@ -0,0 +1,49 @@
+CREATE TYPE event_status AS ENUM ('upcoming', 'ongoing', 'past');
+CREATE TYPE event_type AS ENUM ('Movies', 'Theater', 'Music', 'Sports');
+
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    birthDate DATE,
+    password VARCHAR(255) NOT NULL,
+    profilePic VARCHAR(255),
+    phone VARCHAR(20)
+);
+
+CREATE TABLE Events (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    status event_status NOT NULL,
+    type event_type NOT NULL,
+    host INT NOT NULL,
+    duration VARCHAR(50) NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    ticketPrice DECIMAL(10, 2) NOT NULL CHECK (ticketPrice > 0),
+    FOREIGN KEY (host) REFERENCES Users(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE EventDates (
+    id SERIAL PRIMARY KEY,
+    event_id INT NOT NULL,
+    date DATE NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    seatsAvailable INT NOT NULL CHECK (seatsAvailable > 0),
+    totalSeats INT NOT NULL CHECK (totalSeats > 0),
+    FOREIGN KEY (event_id) REFERENCES Events(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    UNIQUE (event_id, date)
+);
+
+CREATE TABLE Bookings (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    event_id INT NOT NULL,
+    bookingDate DATE NOT NULL, 
+    numOfTickets INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES Events(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,46 @@
 import express from "express"
-import bodyParser from "body-parser"
-import cors from "cors"
-import dotenv from "dotenv"
-import helmet from "helmet"
-import morgan from "morgan"
-import path from "path"
-import { fileURLToPath } from "url"
+// import bodyParser from "body-parser"
+// import cors from "cors"
+// import dotenv from "dotenv"
+// import helmet from "helmet"
+// import morgan from "morgan"
+// import path from "path"
+// import { fileURLToPath } from "url"
 
-import { updateEventStatus } from "./scheduledTaskManager.js"
+// // import { updateEventStatus } from "./scheduledTaskManager.js"
 
-import authRoute from "./routes/authRoute.js"
-import userRoute from "./routes/userRoute.js"
-import eventRoute from "./routes/eventRoute.js"
-import bookingRoute from "./routes/bookingRoute.js"
+// // import authRoute from "./routes/authRoute.js"
+// // import userRoute from "./routes/userRoute.js"
+// // import eventRoute from "./routes/eventRoute.js"
+// // import bookingRoute from "./routes/bookingRoute.js"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-dotenv.config()
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
+// dotenv.config()
 
 const app = express()
 
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
-app.use(morgan("common"))
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors())
-app.use("/assets", express.static(path.join(__dirname, "public/assets")))
+// app.use(express.json())
+// app.use(helmet())
+// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+// app.use(morgan("common"))
+// app.use(bodyParser.json({ limit: "30mb", extended: true }))
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+// app.use(cors())
+// app.use("/assets", express.static(path.join(__dirname, "public/assets")))
 
-// routes
-app.use('/auth', authRoute)
-app.use('/users', userRoute)
-app.use('/events', eventRoute)
-app.use('/bookings', bookingRoute)
+// // // routes
+// // app.use('/auth', authRoute)
+// // app.use('/users', userRoute)
+// // app.use('/events', eventRoute)
+// // app.use('/bookings', bookingRoute)
 
-// scheduled scannings for database updates
-setInterval(updateEventStatus, 24 * 60 * 60 * 1000)
+// // scheduled scannings for database updates
+// // setInterval(updateEventStatus, 24 * 60 * 60 * 1000)
 
-// database connection and server init
+// // database connection and server init
 const PORT = process.env.PORT || 6001
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => {
-	console.log("[Server] Database connected successfully")
-
-	app.listen(PORT, () => {
-		console.log(`[Server] Listening on port ${PORT}`)
-	})
+app.listen(PORT, () => {
+	console.log(`[Server] Listening on port ${PORT}`)
 })
-.catch(error => console.error("[Server] Error: ", error))
